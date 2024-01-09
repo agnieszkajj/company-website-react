9091eda78bb2a8bcc216c0361ddfcb81a302fc0e
Agnieszka <agnieszka.jjjjj@gmail.com>
2024-01-09 13:32:12 +0100

    change width and padding

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -4,12 +4,14 @@ import React from "react";
 
 const Services = (props) => {
   return (
-    <div className="bg-gray-600 h-[90vh]" id="services">
-      <p className="text-3xl font-bold p-10 text-white">What do we do?</p>
-      <div className="grid grid-rows-2 grid-cols-3 gap-10 justify-items-center w-[960px] m-auto mt-5">
-        {offers.map((offer) => {
-          return <SingleOffer offer={offer} key={offer.name}></SingleOffer>;
-        })}
+    <div className="bg-gray-600" id="services">
+      <div className="w-[960px] m-auto">
+        <p className="text-3xl font-bold py-10 text-white">What do we do?</p>
+        <div className="grid grid-rows-2 grid-cols-3 gap-10 justify-items-center mt-5 pb-10">
+          {offers.map((offer) => {
+            return <SingleOffer offer={offer} key={offer.name}></SingleOffer>;
+          })}
+        </div>
       </div>
     </div>
   );
--- src/components/TeamPage/TeamPage.jsx
+++ src/components/TeamPage/TeamPage.jsx
@@ -2,31 +2,35 @@ import React from "react";
 
 const TeamPage = (props) => {
   return (
-    <div className="bg-gray-300 h-screen" id="teamPage">
-      <p className="text-3xl font-bold p-10">Our Team</p>
-      <div className="flex justify-center w-[960px] m-auto flex-col gap-5 p-5">
-        <div className="flex justify-around">
-          <div className="bg-employee1 bg-cover bg-center rounded-full w-[150px] h-[150px]"></div>
-          <div className="w-2/3">
-            <p className="my-3 font-bold">Name Surname [position]</p>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni
-              adipisci voluptatibus animi? Vel provident quibusdam quis placeat,
-              rerum, reprehenderit recusandae aut obcaecati repellendus eligendi
-              consequuntur fugit blanditiis maxime error aspernatur.
-            </p>
+    <div className="bg-gray-300" id="teamPage">
+      <div className="w-[960px] m-auto pb-10">
+        <p className="text-3xl font-bold py-10">Our Team</p>
+        <div className="flex justify-center flex-col gap-10">
+          <div className="flex justify-around">
+            <div className="bg-employee1 bg-cover bg-center rounded-full w-[150px] h-[150px]"></div>
+            <div className="w-2/3">
+              <p className="my-3 font-bold">Name Surname [position]</p>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni
+                adipisci voluptatibus animi? Vel provident quibusdam quis
+                placeat, rerum, reprehenderit recusandae aut obcaecati
+                repellendus eligendi consequuntur fugit blanditiis maxime error
+                aspernatur.
+              </p>
+            </div>
           </div>
-        </div>
-        <div className="flex flex-row-reverse justify-around">
-          <div className="bg-employee2 bg-cover bg-center rounded-full w-[150px] h-[150px]"></div>
-          <div className="w-2/3">
-            <p className="my-3 font-bold">Name Surname [position]</p>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni
-              adipisci voluptatibus animi? Vel provident quibusdam quis placeat,
-              rerum, reprehenderit recusandae aut obcaecati repellendus eligendi
-              consequuntur fugit blanditiis maxime error aspernatur.
-            </p>
+          <div className="flex flex-row-reverse justify-around">
+            <div className="bg-employee2 bg-cover bg-center rounded-full w-[150px] h-[150px]"></div>
+            <div className="w-2/3">
+              <p className="my-3 font-bold">Name Surname [position]</p>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni
+                adipisci voluptatibus animi? Vel provident quibusdam quis
+                placeat, rerum, reprehenderit recusandae aut obcaecati
+                repellendus eligendi consequuntur fugit blanditiis maxime error
+                aspernatur.
+              </p>
+            </div>
           </div>
         </div>
       </div>
