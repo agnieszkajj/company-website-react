81b8e2217a9d1522c93264fabce8a124eecc6f98
Agnieszka <agnieszka.jjjjj@gmail.com>
2024-01-10 09:43:34 +0100

    change width of navbar and footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,16 +2,18 @@ import { FaInstagram, FaFacebookSquare } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div className="flex md:justify-between bg-zinc-700 items-center p-5 flex-col md:flex-row">
-      <div>
-        <p className="font-bold text-white mb-3 md:mb-0">
-          Company name - all rights reserved, 2023
-        </p>
-      </div>
-      <div>
-        <div className="flex gap-5  text-white">
-          <FaInstagram className="text-3xl" />
-          <FaFacebookSquare className="text-3xl" />
+    <div className="bg-zinc-700 ">
+      <div className="flex md:justify-between items-center p-5 flex-col md:flex-row md:w-[960px] m-auto md:px-0">
+        <div>
+          <p className="font-bold text-white mb-3 md:mb-0">
+            Company name - all rights reserved, 2023
+          </p>
+        </div>
+        <div>
+          <div className="flex gap-5  text-white">
+            <FaInstagram className="text-3xl" />
+            <FaFacebookSquare className="text-3xl" />
+          </div>
         </div>
       </div>
     </div>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,18 @@
 const Navbar = () => {
   return (
-    <nav className="flex justify-between bg-zinc-600 h-[10vh]">
-      <div>
-        <p className="font-bold text-white p-5">my company</p>
-      </div>
-      <div>
-        <div className="flex gap-5  text-white p-5 collapse md:visible">
-          <a href="#teamPage">About us</a>
-          <a href="#services">Services</a>
-          <a disabled className="cursor-not-allowed">
-            Contact
-          </a>
+    <nav className=" bg-zinc-600">
+      <div className="flex md:justify-between items-center md:w-[960px] m-auto">
+        <div>
+          <p className="font-bold text-white py-5">my company</p>
+        </div>
+        <div>
+          <div className="flex gap-5  text-white py-5 collapse md:visible">
+            <a href="#teamPage">About us</a>
+            <a href="#services">Services</a>
+            <a disabled className="cursor-not-allowed">
+              Contact
+            </a>
+          </div>
         </div>
       </div>
     </nav>
